960e35e2196d0db69c057c91a59aade50d2ca248
alexeh <alexurola@gmail.com>
2024-04-07 09:57:08 +0300

    add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+
+import { withContentlayer } from "next-contentlayer";
+
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    // experimental: {
+    //     mdxRs: true,
+    // },
+    output: 'export',
+};
+
+export default withContentlayer(nextConfig as any);
