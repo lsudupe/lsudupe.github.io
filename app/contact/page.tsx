236e7eedca89bf451f9dfe20fccee977916d236a
alexeh <alexurola@gmail.com>
2024-04-08 06:44:11 +0300

    update contacts and favicons

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,28 +1,35 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import {FileArchiveIcon, Github, Linkedin, Mail, Twitter,} from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/lsudupe001/",
+		label: "LinkedIn",
+		handle: "Laura Sudupe",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:sudupelaura@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/lsudupe",
 		label: "Github",
-		handle: "chronark",
+		handle: "lsudupe",
 	},
+	{
+		icon: <FileArchiveIcon size={20} />,
+		href: "/cv.pdf",
+		label: "Resume",
+		handle: "Resume",
+	},
+
 ];
 
 export default function Example() {
@@ -30,7 +37,7 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-4 lg:gap-16">
 					{socials.map((s) => (
 						<Card>
 							<Link
